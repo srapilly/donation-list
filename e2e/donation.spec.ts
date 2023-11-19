052396b900e5727416e92554cb53088626fb59cf
Simon Rapilly <85184231+srapilly@users.noreply.github.com>
2023-11-19 12:22:19 -0500

    Donation table

--- e2e/donation.spec.ts
+++ e2e/donation.spec.ts
@@ -5,3 +5,14 @@ test("has title", async ({ page }) => {
 
   await expect(page).toHaveTitle(/Donation - Zeffy/);
 });
+
+test("has donations", async ({ page }) => {
+  await page.goto("/donation");
+
+  await expect(page.getByRole("heading", { name: "Donations" })).toBeVisible();
+
+  await expect(page.getByRole("row")).toHaveCount(101);
+  await expect(
+    page.getByRole("row").filter({ hasText: "1684766860371" + "G F" + "3000" }),
+  ).toBeVisible();
+});
--- src/routes/Donation.tsx
+++ src/routes/Donation.tsx
@@ -1,7 +1,34 @@
 import { usePage } from "../hooks/usePage";
+import donations from "../assets/donations.json";
 
 export function Donation() {
   usePage("Donation");
 
-  return <h1>Donations</h1>;
+  return (
+    <>
+      <h1>Donations</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Name</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {donations.map(({ id, donation }) => {
+            return (
+              <tr key={id}>
+                <td>{donation.createdAtUtc}</td>
+                <td>
+                  {donation.firstName} {donation.lastName}
+                </td>
+                <td>{donation.amount}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
