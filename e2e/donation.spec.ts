5f6823b19f43583191f4be1ddac8a866c9aa6323
Simon Rapilly <85184231+srapilly@users.noreply.github.com>
2023-11-20 00:11:58 -0500

    Pagination (#5)

--- e2e/donation.spec.ts
+++ e2e/donation.spec.ts
@@ -12,8 +12,26 @@ test("has donations", async ({ page }) => {
 
   await expect(page.getByRole("heading", { name: "Donations" })).toBeVisible();
 
-  await expect(page.getByRole("row")).toHaveCount(101);
+  await expect(page.getByRole("row")).toHaveCount(51);
   await expect(
     page.getByRole("row").filter({ hasText: "22 May, 16:47" + "G F" + "$30" }),
   ).toBeVisible();
+});
+
+test("has pagination", async ({ page }) => {
+  await page.goto("/donation");
+
+  await expect(page.getByRole("link", { name: "Next" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Previous" })).not.toBeVisible();
+
+  await page.getByRole("link", { name: "Next" }).click();
+
+  await expect(
+    page
+      .getByRole("row")
+      .filter({ hasText: "19 May, 02:40" + "T N" + "$2175" }),
+  ).toBeVisible();
+
+  await expect(page.getByRole("link", { name: "Next" })).not.toBeVisible();
+  await expect(page.getByRole("link", { name: "Previous" })).toBeVisible();
 });
